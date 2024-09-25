556371fd29de713a8ff524aeca81ad54ee951105
Enkre7 <victor.mairot@proton.me>
2024-09-25 13:13:50 +0200

    Automatic commit

--- configModules/nixos.nix
+++ configModules/nixos.nix
@@ -4,6 +4,13 @@
   nix = {
     settings.experimental-features = [ "nix-command" "flakes" ];
     settings.auto-optimise-store = true;
+    settings.substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    settings.trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
     optimise.automatic = true;
     /*gc = {
       automatic = true;
