4a92e7038d5720181b45857d3ab3b03ef484bfb7
Enkre7 <victor.mairot@proton.me>
2024-10-19 17:05:39 +0200

    Automatic commit

--- homeModules/hyprland.nix
+++ homeModules/hyprland.nix
@@ -18,7 +18,7 @@ let
     ${pkgs.webcord}/bin/webcord -m &
     ${pkgs.mullvad-vpn}/bin/mullvad-vpn &
     ${pkgs.nextcloud-client}/bin/nextcloud --background &
-    ${pkgs.openrgb}/bin/openrgb --startminimized -m static -c 00FF00 &
+    ${pkgs.openrgb}/bin/openrgb --startminimized -m static -c 00FF00 -b 100 &
   '';
 in
 {
