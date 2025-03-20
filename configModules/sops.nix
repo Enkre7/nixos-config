bf39bba30ce9e1f5c3aeb059bcf5553a2ec16fe7
Enkre7 <victor.mairot@proton.me>
2025-03-21 00:05:47 +0100

    Automatic commit

--- configModules/sops.nix
+++ configModules/sops.nix
@@ -6,4 +6,14 @@
   sops.defaultSopsFile = ./secrets.yaml;
   sops.defaultSopsFormat = "yaml";
   sops.age.generateKey = false;
+  sops.age.keyFile = "/home/${config.user}/.config/sops/age/keys.txt";
+
+  environment.variables = {
+    SOPS_AGE_KEY_FILE = config.sops.age.keyFile;
+  };
+
+  systemd.services.pcscd = {
+    wantedBy = [ "multi-user.target" ];
+    before = [ "sops-nix.service" ];
+  };
 }
