e145b76d7238ac6daf789c1d9e623c9f8938c30e
Enkre7 <victor.mairot@proton.me>
2025-05-12 18:17:41 +0200

    Automatic commit

--- homeModules/notification.nix
+++ homeModules/notification.nix
@@ -3,9 +3,10 @@
 {
   services.mako = {
     enable = true;
-    borderRadius = 10;
-    defaultTimeout = 1000;
+    settings = {
+      border-radius = 10;
+      default-timeout = 1000;
+    };
   };
-
   home.packages = with pkgs; [ libnotify ];
 }
