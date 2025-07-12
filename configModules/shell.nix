4e658cec1c9262cc8bc28ee7df3d9e8d032119a4
Enkre7 <victor.mairot@proton.me>
2025-07-12 12:34:59 +0200

    Automatic commit

--- configModules/shell.nix
+++ configModules/shell.nix
@@ -4,4 +4,12 @@
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh bash ];
   programs.zsh.enable = true; # Enable nix-zsh-completions plugin
+
+  services.journald = {
+    extraConfig = ''
+      MaxRetentionSec=1month
+      SystemMaxUse=1G
+      SystemMaxFileSize=100M
+    '';
+  };
 }
