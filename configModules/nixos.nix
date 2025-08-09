7a4f22ff2f4a1bb26a89c30c5467b6cd0e1ea913
Enkre7 <victor.mairot@proton.me>
2025-08-09 21:50:50 +0200

    Automatic commit

--- configModules/nixos.nix
+++ configModules/nixos.nix
@@ -12,6 +12,7 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     optimise.automatic = true;
+    settings.warn-dirty = false;
   };
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = config.stateVersion;
