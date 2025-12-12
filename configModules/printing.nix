0cb654d282ee11c6503589a5d7b5a41421ef4419
Enkre7 <victor.mairot@proton.me>
2025-12-12 12:23:21 +0100

    Automatic commit

--- configModules/printing.nix
+++ configModules/printing.nix
@@ -1,12 +1,20 @@
 { config, pkgs, ... }:
 
 {
+  # Printing
   services.printing = {
     enable = true;
     drivers = with pkgs; [ brgenml1cupswrapper gutenprint ];
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
   };
+  
   programs.system-config-printer.enable = true;
   
+  # Scanner
   hardware.sane = {
     enable = true;
     brscan4 = {
@@ -20,6 +28,9 @@
     };
   };
   
+  services.ipp-usb.enable = true;
+  
+  # Both
   services.avahi = {
     enable = true;
     nssmdns4 = true;
@@ -29,16 +40,14 @@
       userServices = true;
     };
   };
-  services.printing = {
-    listenAddresses = [ "*:631" ];
-    allowFrom = [ "all" ];
-    browsing = true;
-    defaultShared = true;
-    openFirewall = true;
-  };
   
-  services.ipp-usb.enable = true;
   users.users.${config.user}.extraGroups = [ "scanner" "lp" ];
 
-  environment.systemPackages = with pkgs; [ simple-scan system-config-printer gscan2pdf ];
+  environment.systemPackages = with pkgs; [ 
+    # Printing
+    system-config-printer
+    # Scanner
+    simple-scan
+    gscan2pdf
+  ];
 }
