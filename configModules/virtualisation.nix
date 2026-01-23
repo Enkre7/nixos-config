faf34bee9ebf64ebbfa7273f3be5414be6c95414
Enkre7 <victor.mairot@proton.me>
2026-01-23 10:00:52 +0100

    Automatic commit

--- configModules/virtualisation.nix
+++ configModules/virtualisation.nix
@@ -1,9 +1,7 @@
 { config, pkgs, ...}:
-
 {
   # System Management Tool
   programs.dconf.enable = true;
-
   # Libvirtd
   boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
   virtualisation = {
@@ -25,4 +23,45 @@
     virtio-win
     virtiofsd
   ];
+  # VirtIO-FS service for Downloads sharing
+  systemd.services.virtiofsd-downloads = {
+    description = "VirtIO-FS daemon for Downloads folder";
+    after = [ "local-fs.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.virtiofsd}/bin/virtiofsd --socket-path=/var/lib/libvirt/virtiofsd-downloads.sock --shared-dir=/home/enkre/Downloads --cache=auto";
+      Restart = "always";
+      User = "root";
+    };
+  };
+
+  # Samba for Windows VMs
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "NixOS Samba Server";
+        "netbios name" = "promethium";
+        "security" = "user";
+      };
+      downloads = {
+        path = "/home/enkre/Downloads";
+        browseable = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "valid users" = "enkre";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+      };
+    };
+  };
+
+  # Firewall for libvirt network
+  networking.firewall.interfaces."virbr0" = {
+    allowedTCPPorts = [ 139 445 ];
+    allowedUDPPorts = [ 137 138 ];
+  };
 }
