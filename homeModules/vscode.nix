2064caee4a9c27adc9f42ab05decaf0a64de94a4
Enkre7 <victor.mairot@proton.me>
2026-03-30 20:04:30 +0200

    Automatic commit

--- homeModules/vscode.nix
+++ homeModules/vscode.nix
@@ -10,6 +10,7 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
+    mutableExtensionsDir = false;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       jdinhlife.gruvbox
       ms-vscode.cpptools-extension-pack
